34cfb63fea1bcbf277a8fd00e2f120c14373ee41
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2019-04-01 12:31:55 +0530

    Modified: static build path

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const testSchema = new mongoose.Schema({
 });
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 const newRecordModel = mongoose.model("newRecordModel", testSchema);
